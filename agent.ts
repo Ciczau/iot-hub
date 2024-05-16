ae49b5f42d641529cfce2a51a2a8618d328898a4
Ciczau <wiktor.michalski@outlook.com>
2024-05-16 20:57:07 +0200

    connection with opcua client

--- agent.ts
+++ agent.ts
@@ -1,5 +1,23 @@
+import { OPCUAClient } from "node-opcua";
+
+const opcuaClient = OPCUAClient.create({ endpointMustExist: false });
+const opcuaEndpointUrl = "opc.tcp://localhost:4840";
+
 async function main() {
-  console.log("working app");
+  try {
+    await opcuaClient.connect(opcuaEndpointUrl);
+    const session = await opcuaClient.createSession();
+
+    const subscription = await session.createSubscription2({
+      requestedPublishingInterval: 1000,
+      requestedMaxKeepAliveCount: 10,
+      maxNotificationsPerPublish: 10,
+      publishingEnabled: true,
+      priority: 10,
+    });
+  } catch (err) {
+    console.error("Error in OPC UA client setup:", err);
+  }
 }
 
 main();
